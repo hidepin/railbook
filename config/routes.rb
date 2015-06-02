95e8cf758f448bfea038e22751ff28eca62a66c8
Hidetoshi Imai <hidepin@gmail.com>
2015-06-02 22:39:15 +0900

    2.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'hello/show'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  match ':controller(/:action(/:id))', via: [ :get, :post, :patch ]
 end
