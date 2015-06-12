2ff082ebd80fd5a55b9a71221060be92bccb6b76
Hidetoshi Imai <hidepin@gmail.com>
2015-06-13 08:18:00 +0900

    4.3.2

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -26,4 +26,7 @@ class ViewController < ApplicationController
     @book = Book.new(publish: '技術評論社')
     @books = Book.select(:publish).distinct
   end
+  def default_url_options(options = {})
+    { charset: 'utf-8' }
+  end
 end
