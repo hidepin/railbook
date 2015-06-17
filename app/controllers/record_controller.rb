e53e887e049f7ba70e84af14096951a0b5603e1a
Hidetoshi Imai <hidepin@gmail.com>
2015-06-18 08:35:32 +0900

    5.2.16
    Signed-off-by: Hidetoshi Imai <hidepin@gmail.com>

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -94,4 +94,18 @@ class RecordController < ApplicationController
       .unscope(where: :cd)
     render 'books/index'
   end
+
+  def none
+    case params[:id]
+    when 'all'
+      @books = Book.all
+    when 'new'
+      @books = Book.order('published DESC').limit(5)
+    when 'cheap'
+      @books = Book.order(:price).limit(5)
+    else
+      @books = Book.none
+    end
+    render 'books/index'
+  end
 end
