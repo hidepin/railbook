766a40346e72fe8488ed9121abf11114130b181e
Hidetoshi Imai <hidepin@gmail.com>
2015-06-25 00:53:01 +0900

    5.3.7

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -143,4 +143,9 @@ class RecordController < ApplicationController
   def groupby2
     @books = Book.group(:publish).average(:price)
   end
+
+  def literal_sql
+    @books = Book.find_by_sql(['SELECT publish, AVG(price) AS avg_price FROM "books" GROUP BY publish HAVING AVG(price) >= ?', 2500])
+    render 'record/groupby'
+  end
 end
