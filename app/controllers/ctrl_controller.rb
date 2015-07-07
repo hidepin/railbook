c720c5418f5d61996a5774ea83bbb72f9068d432
Hidetoshi Imai <hidepin@gmail.com>
2015-07-08 04:17:33 +0900

    6.1.1

--- app/controllers/ctrl_controller.rb
+++ app/controllers/ctrl_controller.rb
@@ -1,2 +1,10 @@
+# -*- coding: utf-8 -*-
 class CtrlController < ApplicationController
+  def para
+    render text: 'idパラメータ : ' + params[:id]
+  end
+
+  def para_array
+    render text: 'categoryパラメータ : ' + params[:category].inspect
+  end
 end
